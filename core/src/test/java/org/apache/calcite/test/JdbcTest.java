b940f059b1cd7f6c7ffa1e0b84349cb9102c9cf2
Diveyam Mishra <mishradiveyam@gmail.com>
2026-09-05 16:30:37 +0530

    [CALCITE-7765] Re-enable disabled tests in RelMetadataTest and migrate multi-table join test to Quidem

--- core/src/test/java/org/apache/calcite/test/JdbcTest.java
+++ core/src/test/java/org/apache/calcite/test/JdbcTest.java
@@ -1247,47 +1247,6 @@ public class JdbcTest {
             + "the_year=1998; C=365; M=April\n");
   }
 
-  @Disabled("The test returns expected results. Not sure why it is disabled")
-  @Test void testCloneGroupBy2() {
-    CalciteAssert.that()
-        .with(CalciteAssert.Config.FOODMART_CLONE)
-        .query(
-            "select \"time_by_day\".\"the_year\" as \"c0\", \"time_by_day\".\"quarter\" as \"c1\", \"product_class\".\"product_family\" as \"c2\", sum(\"sales_fact_1997\".\"unit_sales\") as \"m0\" from \"time_by_day\" as \"time_by_day\", \"sales_fact_1997\" as \"sales_fact_1997\", \"product_class\" as \"product_class\", \"product\" as \"product\" where \"sales_fact_1997\".\"time_id\" = \"time_by_day\".\"time_id\" and \"time_by_day\".\"the_year\" = 1997 and \"sales_fact_1997\".\"product_id\" = \"product\".\"product_id\" and \"product\".\"product_class_id\" = \"product_class\".\"product_class_id\" group by \"time_by_day\".\"the_year\", \"time_by_day\".\"quarter\", \"product_class\".\"product_family\"")
-        .returnsUnordered(
-            "c0=1997; c1=Q2; c2=Drink; m0=5895.0000",
-            "c0=1997; c1=Q1; c2=Food; m0=47809.0000",
-            "c0=1997; c1=Q3; c2=Drink; m0=6065.0000",
-            "c0=1997; c1=Q4; c2=Drink; m0=6661.0000",
-            "c0=1997; c1=Q4; c2=Food; m0=51866.0000",
-            "c0=1997; c1=Q1; c2=Drink; m0=5976.0000",
-            "c0=1997; c1=Q3; c2=Non-Consumable; m0=12343.0000",
-            "c0=1997; c1=Q4; c2=Non-Consumable; m0=13497.0000",
-            "c0=1997; c1=Q2; c2=Non-Consumable; m0=11890.0000",
-            "c0=1997; c1=Q2; c2=Food; m0=44825.0000",
-            "c0=1997; c1=Q3; c2=Food; m0=47440.0000",
-            "c0=1997; c1=Q1; c2=Non-Consumable; m0=12506.0000");
-  }
-
-  /** Tests plan for a query with 4 tables, 3 joins. */
-  @Disabled("The actual and expected plan differ")
-  @Test void testCloneGroupBy2Plan() {
-    CalciteAssert.that()
-        .with(CalciteAssert.Config.FOODMART_CLONE)
-        .query(
-            "explain plan for select \"time_by_day\".\"the_year\" as \"c0\", \"time_by_day\".\"quarter\" as \"c1\", \"product_class\".\"product_family\" as \"c2\", sum(\"sales_fact_1997\".\"unit_sales\") as \"m0\" from \"time_by_day\" as \"time_by_day\", \"sales_fact_1997\" as \"sales_fact_1997\", \"product_class\" as \"product_class\", \"product\" as \"product\" where \"sales_fact_1997\".\"time_id\" = \"time_by_day\".\"time_id\" and \"time_by_day\".\"the_year\" = 1997 and \"sales_fact_1997\".\"product_id\" = \"product\".\"product_id\" and \"product\".\"product_class_id\" = \"product_class\".\"product_class_id\" group by \"time_by_day\".\"the_year\", \"time_by_day\".\"quarter\", \"product_class\".\"product_family\"")
-        .returns("PLAN=EnumerableAggregate(group=[{0, 1, 2}], m0=[SUM($3)])\n"
-            + "  EnumerableCalc(expr#0..37=[{inputs}], c0=[$t9], c1=[$t13], c2=[$t4], unit_sales=[$t22])\n"
-            + "    EnumerableHashJoin(condition=[=($23, $0)], joinType=[inner])\n"
-            + "      EnumerableTableScan(table=[[foodmart2, product_class]])\n"
-            + "      EnumerableHashJoin(condition=[=($10, $19)], joinType=[inner])\n"
-            + "        EnumerableHashJoin(condition=[=($11, $0)], joinType=[inner])\n"
-            + "          EnumerableCalc(expr#0..9=[{inputs}], expr#10=[CAST($t4):INTEGER], expr#11=[1997], expr#12=[=($t10, $t11)], proj#0..9=[{exprs}], $condition=[$t12])\n"
-            + "            EnumerableTableScan(table=[[foodmart2, time_by_day]])\n"
-            + "          EnumerableTableScan(table=[[foodmart2, sales_fact_1997]])\n"
-            + "        EnumerableTableScan(table=[[foodmart2, product]])\n"
-            + "\n");
-  }
-
   @Test void testOrderByCase() {
     CalciteAssert.that()
         .with(CalciteAssert.Config.FOODMART_CLONE)
